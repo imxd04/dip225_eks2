a513980aa9cc29a67614419189d59799757bb729
imxd04 <imxd690@gmail.com>
2025-06-17 21:53:15 +0300

    Question 4

---
+++ task 4.py
@@ -0,0 +1,22 @@
+from openpyxl import load_workbook
+
+wb = load_workbook('sagatave_eksamenam.xlsx')
+ws = wb['Lapa_0']
+max_row = ws.max_row
+
+total_sum = 0
+count = 0
+
+for row in range(2, max_row + 1):
+    product = ws['I' + str(row)].value
+    price = ws['K' + str(row)].value
+
+    if isinstance(product, str) and 'LaserJet' in product and isinstance(price, (int, float)):
+        total_sum += price
+        count += 1
+
+if count > 0:
+    average = int(total_sum / count)
+    print(average)
+else:
+    print(0)
